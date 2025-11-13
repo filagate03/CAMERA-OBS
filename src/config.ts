8425e9c5c8351dcb5208f288a9a1c087b539a429
filagate03 <filagate03@gmail.com>
2025-11-13 06:16:36 +0300

    feat: add public turn servers

--- src/config.ts
+++ src/config.ts
@@ -5,6 +5,16 @@ export const ICE_SERVERS: RTCIceServer[] = [
   { urls: 'stun:stun.l.google.com:19302' },
   { urls: 'stun:stun1.l.google.com:19302' },
   { urls: 'stun:stun2.l.google.com:19302' },
+  {
+    urls: ['turn:global.relay.metered.ca:80', 'turn:global.relay.metered.ca:443'],
+    username: 'openrelayproject',
+    credential: 'openrelayproject',
+  },
+  {
+    urls: ['turns:global.relay.metered.ca:80', 'turns:global.relay.metered.ca:443'],
+    username: 'openrelayproject',
+    credential: 'openrelayproject',
+  },
 ];
 
 export const MEDIA_CONSTRAINTS: MediaStreamConstraints = {
